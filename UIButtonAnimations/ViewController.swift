43e1266cf1a93e173dfd85b93a328a71944406b1
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-11 02:33:06 +0200

    Add UI, functionality and animation

--- UIButtonAnimations/ViewController.swift
+++ UIButtonAnimations/ViewController.swift
@@ -1,19 +1,49 @@
-//
-//  ViewController.swift
-//  UIButtonAnimations
-//
-//  Created by Office-iMac on 2020-12-11.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+extension UIButton {
+    func pulsate() {
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.4
+        pulse.fromValue = 0.98
+        pulse.toValue = 1.0
+        pulse.autoreverses = true
+        pulse.repeatCount = 3
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        layer.add(pulse, forKey: nil)
+    }
+    func flash() {
+        let flash = CABasicAnimation(keyPath: "opacity")
+        flash.duration = 0.3
+        flash.fromValue = 1
+        flash.toValue = 0.1
+        flash.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        flash.autoreverses = true
+        flash.repeatCount = 1
+        layer.add(flash, forKey: nil)
+    }
+    
+    
+}
 
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func flashButtonTapped(_ sender: UIButton) {
+        sender.flash()
+    }
+    
+    
+    @IBAction func pulseButtonTapped(_ sender: UIButton) {
+        sender.pulsate()
+    }
+    
+    
 }
 
